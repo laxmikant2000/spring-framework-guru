a0c7b39c2fd8c17760f80eb7ec1397dedfd48060
Laxmikant Katre <57556260+laxmikant2000@users.noreply.github.com>
2023-10-11 11:56:48 +0530

    Delete 1) spring-6-webapp directory

--- 1) spring-6-webapp/spring-6-webapp/src/main/java/guru/springframework/spring6webapp/service/AuthorService.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.spring6webapp.service;
-
-import guru.springframework.spring6webapp.domain.Author;
-
-public interface AuthorService {
-
-    Iterable<Author> findAll();
-}
